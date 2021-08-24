ecd9a48686c57c3335a76f05947022b204bf7fc8
Angela Wang <wangela472@gmail.com>
2021-08-24 02:24:15 -0400

    Initialize empty pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Title from "./sections/Title";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
   );
 }
 
---
+++ src/sections/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+          TITLE  
+        </div>
+    )
+}
+
+export default Title
