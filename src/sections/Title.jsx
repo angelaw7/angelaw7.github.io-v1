2de9132cc9812585baf791077789e41c364c61e1
Angela Wang <wangela472@gmail.com>
2021-08-24 04:06:12 -0400

    Add background image and style section

--- src/sections/Title.jsx
+++ src/sections/Title.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
-import { IonTitle } from '@ionic/react'
+import styled from 'styled-components'
+import background from "../imgs/bgimg.png"
+
+const BgImg = styled.div`
+background-image: linear-gradient( rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45) ),url(${background});
+background-size: cover;
+min-height: 100vh;
+background-repeat: no-repeat;
+`
+
+const TextContainer = styled.div`
+margin: auto;
+width: 50%;
+`
+
+const NameTitle = styled.h1`
+margin: auto;
+color: white;
+padding-top: 30vh;
+font-family: 'Viga', sans-serif;
+font-size: 48px;
+`
+
+const Blue = styled.span`
+color: #9DD6FF;
+`
+
+const SubHeading = styled.p`
+color: white;
+`
+
+const LinkContainer = styled.div`
+display: flex;
+justify-content: space-between;
+max-width: 300px;
+`
+
 const Title = () => {
   return (
-    <div>
-      <IonTitle>
-        Hi!<br/>I'm Angela
-      </IonTitle>
-    </div>
+    <BgImg>
+      <TextContainer>
+        <NameTitle>
+          Hi!<br />I'm <Blue>Angela</Blue>
+        </NameTitle>
+        <SubHeading>Software & Biomedical Engineering Student</SubHeading>
+        <LinkContainer>
+          <SubHeading>/about</SubHeading>
+          <SubHeading>/projects</SubHeading>
+          <SubHeading>/contact</SubHeading>
+        </LinkContainer>
+      </TextContainer>
+    </BgImg>
   )
 }
 
