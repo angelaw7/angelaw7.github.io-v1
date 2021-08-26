8df8b7ecc05f11f2a3cb5a2d64628df78f27925f
Angela Wang <wangela472@gmail.com>
2021-08-26 18:56:20 -0400

    Set up contact form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,47 @@
 import React from 'react'
 import PageContainer from '../components/PageContainer'
 import { useForm, ValidationError } from '@formspree/react';
+import styled from 'styled-components';
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	width: 80%;
+	margin: auto;
+`
+
+const ContactEmail = styled.input`
+	border-radius: 6px;
+	margin-bottom: 10px;
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	border: 0;
+	padding: 10px;
+	&:focus {
+		outline: none;
+	}
+`
+
+const ContactMsg = styled.textarea`
+	border-radius: 6px;
+	margin-bottom: 10px;
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	border: 0;
+	padding: 10px;
+	height: 100px;
+	&:focus {
+		outline: none;
+	}
+`
+
+const ContactBtn = styled.button`
+	border-radius: 6px;
+	padding: 8px;
+	border: 0;
+	background-color: #4CAF50;
+	color: white;
+	font-size: 16px;
+	cursor: pointer;
+`
 
 const Contact = () => {
 	const [state, handleSubmit] = useForm("mjvjpnlp");
@@ -16,33 +57,36 @@ const Contact = () => {
 						</p>
 						<h2>Let's Connect!</h2>
 					</div>
-					<div style={{ width: "50%" }}>
-						{state.succeeded ? <p>Thanks for your message! I'll get back to you shortly.</p> : <form onSubmit={handleSubmit}>
-							<input
+					<div style={{ width: "50%", display: "flex" }}>
+						{state.succeeded ? <p>Thanks for your message! I'll get back to you shortly.</p> : <Form onSubmit={handleSubmit} >
+							<input name="_subject" type="hidden" value="New message from personal website contact form" />
+							<ContactEmail
 								id="email"
 								type="email"
 								name="email"
 								placeholder="Email Address"
+								required
 							/>
 							<ValidationError
 								prefix="Email"
 								field="email"
 								errors={state.errors}
 							/>
-							<textarea
+							<ContactMsg
 								id="message"
 								name="message"
 								placeholder="Message"
+								required
 							/>
 							<ValidationError
 								prefix="Message"
 								field="message"
 								errors={state.errors}
 							/>
-							<button type="submit" disabled={state.submitting}>
+							<ContactBtn type="submit" disabled={state.submitting}>
 								Submit
-							</button>
-						</form>}
+							</ContactBtn>
+						</Form>}
 					</div>
 				</div>
 			</PageContainer>
