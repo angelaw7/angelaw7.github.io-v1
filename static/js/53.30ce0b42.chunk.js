b0354cd0f39ce8f2df254633845fe8aace20b8b5
Angela Wang <wangela472@gmail.com>
2021-08-27 04:05:53 -0400

    Updates

---
+++ static/js/53.30ce0b42.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[53],{121:function(t,n,o){"use strict";o.r(n),o.d(n,"startStatusTap",(function(){return r}));var e=o(15),i=o(11),r=function(){var t=window;t.addEventListener("statusTap",(function(){Object(e.h)((function(){var n=t.innerWidth,o=t.innerHeight,r=document.elementFromPoint(n/2,o/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(t){return Object(i.c)(c,t)})).then((function(){Object(e.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.30ce0b42.chunk.js.map
