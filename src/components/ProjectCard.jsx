2fb742ec495909ace21334a53981da037462cdeb
Angela Wang <wangela472@gmail.com>
2021-08-27 03:44:35 -0400

    Fix links and add DevUp

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,33 +1,47 @@
 import React from 'react'
-import { IonImg, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCol } from '@ionic/react';
+import { IonImg, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCol, IonText, IonCardSubtitle } from '@ionic/react';
 import styled from 'styled-components';
 
-const ProjectCard = ({ title, img, desc, link }) => {
-    const handleClick = (link) => {
-        const newWindow = window.open(link, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-    }
+const ProjectCard = ({ title, img, desc, website, repo }) => {
 
     return (
         <IonCol style={{ maxWidth: "25vw", minWidth: "20vw", cursor: "pointer" }}>
-            <Card onClick={() => handleClick(link)}>
+            <Card >
                 <IonCardHeader>
-                    <IonCardTitle>{title}</IonCardTitle>
+                    <IonCardTitle >{title}</IonCardTitle>
                 </IonCardHeader>
                 <Image style={{ padding: '0px 15px' }} src={img} />
                 <IonCardContent>
-                    {desc}
+                    <IonText>
+                        {desc}
+                    </IonText>
+                    <Links >
+                        {website ? <Link href={website} target="_blank" rel="noreferrer">Visit the Website</Link> : ""}
+                        {repo ? <Link href={repo} target="_blank" rel="noreferrer">GitHub Repo</Link> : ""}
+                    </Links>
                 </IonCardContent>
             </Card>
         </IonCol>
     )
 }
 
+const Link = styled.a`
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`
+
+const Links = styled(IonCardSubtitle)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 16px 0;
+`
+
 const Image = styled(IonImg)`
     padding: 0px 15px;
 `
 
 const Card = styled(IonCard)`
+    cursor: default;
     &:hover {
         background-color: #fff4f4;
     }    
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import PageContainer from '../components/PageContainer'
 import { IonRow } from '@ionic/react'
 import ProjectCard from '../components/ProjectCard'
-import project1img from '../imgs/bgimg.png';
+import placeholderimg from '../imgs/bgimg.png';
+import devupImg from '../imgs/DevUp.png';
 
 const projects = [
-    { id: 1, title: "Sample", img: project1img, desc: "Sample description sample description sample description sample description ", link: "https://google.ca" },
-    { id: 2, title: "Sample", img: project1img, desc: "Sample description sample description sample description sample description ", link: "https://google.ca" },
+    {
+        id: 1, title: "DevUp", img: devupImg,
+        desc: "A CRUD web application built with a Flask backend and React frontend. Purpose is to help developers find others to collaborate with on side projects. Hosted on an AWS EC2 instance using Docker containers. Implemented CI/CD with GitHub Actions, frontend and backend testing, and monitoring with cAdvisor, Grafana, and Prometheus.",
+        repo: "https://github.com/LaloVene/MLH-PE-Project",
+        website: "https://dev-up.tech"
+    },
+    // { id: 2, title: "Sample", img: placeholderimg, desc: "Sample description sample description sample description sample description ", link: "https://google.ca" },
 
 ]
 
@@ -16,7 +22,7 @@ const Projects = () => {
             <PageContainer>
                 <h1>Projects</h1>
                 <IonRow>
-                    {projects.map(project => { return (<ProjectCard key={project.id} title={project.title} img={project.img} desc={project.desc} link={project.link} />) })}
+                    {projects.map(project => { return (<ProjectCard key={project.id} title={project.title} img={project.img} desc={project.desc} website={project.website} repo={project.repo} />) })}
                 </IonRow>
             </PageContainer>
         </div>
