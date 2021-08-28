c3f34d56443575fd34c4b3c1c5ad71c98667115c
Angela Wang <wangela472@gmail.com>
2021-08-28 01:53:03 -0400

    Add react-icons and new skills

--- src/components/SkillsList.jsx
+++ src/components/SkillsList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { IonCol, IonIcon, IonRow, IonText } from '@ionic/react'
 import { logoHtml5, logoCss3, logoReact, logoDocker, logoPython, logoJavascript, logoGithub, logoTux, logoWordpress } from 'ionicons/icons';
+import { SiFlask, SiVisualstudiocode, SiGit, SiFigma, SiAmazonaws, SiRaspberrypi } from 'react-icons/si'
+import { HiDatabase } from 'react-icons/hi'
 
 const skills = [
     { name: "Python", logo: logoPython },
@@ -15,16 +17,55 @@ const skills = [
 ]
 
 const SkillsList = () => {
+
+    const Col = (props) => {
+        return (
+            <IonCol
+                style={{ alignItems: "center", flexDirection: "column", display: "flex", padding: "10px" }}
+                sizeSm={3}
+                sizeMd={1.5}
+            >
+                {props.children}
+            </IonCol>
+        )
+    }
+
     return (
-        <IonRow>
+        <IonRow style={{ alignItems: 'center' }} >
             {skills.map((skill) => (
-                <IonCol key={skill.name} >
-                    <div style={{ alignItems: "center", flexDirection: "column", display: "flex" }}>
-                        <IonIcon icon={skill.logo} style={{ pointerEvents: 'none' }} size="large" />
-                        <IonText >{skill.name}</IonText>
-                    </div>
-                </IonCol>
+                <Col key={skill.name} >
+                    <IonIcon icon={skill.logo} style={{ pointerEvents: 'none' }} size="large" />
+                    <IonText >{skill.name}</IonText>
+                </Col>
             ))}
+            <Col >
+                <HiDatabase size="30" />
+                SQL
+            </Col>
+            <Col >
+                <SiFlask size="30" />
+                Flask
+            </Col>
+            <Col >
+                <SiVisualstudiocode size="30" />
+                VSCode
+            </Col>
+            <Col >
+                <SiGit size="30" />
+                Git
+            </Col>
+            <Col >
+                <SiAmazonaws size="30" />
+                AWS
+            </Col>
+            <Col >
+                <SiRaspberrypi size="30" />
+                Raspberry Pi
+            </Col>
+            <Col >
+                <SiFigma size="30" />
+                Figma
+            </Col>
         </IonRow>
     )
 }
